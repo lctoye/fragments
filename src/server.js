389223b9ed2b61f24093bab770eebfc831230182
Liam Toye <lctoye@myseneca.ca>
2024-05-19 21:30:54 -0400

    Add dotenv

--- src/server.js
+++ src/server.js
@@ -9,8 +9,13 @@ const logger = require('./logger');
 // Get our express app instance
 const app = require('./app');
 
-// Get the desired port from the process' environment. Default to `8080`
+// Get the desired port and log level from the process' environment. Default to `8080`
 const port = parseInt(process.env.PORT || '8080', 10);
+const logLevel = process.env.LOG_LEVEL || 'info';
+
+if (logLevel === 'debug') {
+  logger.debug({ env: process.env }, 'Environment variables:');
+}
 
 // Start a server listening on this port
 const server = stoppable(
