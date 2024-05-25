59fff57d0afe63e24f2c4e5fa24ff72471184231
Liam Toye <lctoye@myseneca.ca>
2024-05-25 15:10:01 -0400

    404 test

---
+++ tests/unit/app.test.js
@@ -0,0 +1,12 @@
+// tests/unit/app.test.js
+
+const request = require('supertest');
+
+const app = require('../../src/app');
+
+describe('Test for 404 response', () => {
+  test('should return 404 response', async () => {
+    const res = await request(app).get('/nonexistent');
+    expect(res.statusCode).toBe(404);
+  });
+});
