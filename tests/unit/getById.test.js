9b77ed2cb87230f5ba99566f69baf6a4a4581b64
Liam Toye <lctoye@myseneca.ca>
2024-07-21 02:19:24 -0400

    Lab 8

--- tests/unit/getById.test.js
+++ tests/unit/getById.test.js
@@ -1,33 +1,7 @@
 const app = require('../../src/app');
 const request = require('supertest');
-// const { Fragment } = require("../../src/model/fragment");
 
 describe('Authentication Tests', () => {
-  // test('missing auth is denied', () => request(app).get('/v1/fragments/testid').expect(401));
-
-  // test('incorrect credentials are denied', () =>
-  //   request(app).get('/v1/fragments/testid').auth('incorrect', 'credentials').expect(401));
-
-  // test('authenticated users receive fragment', async () => {
-  //   const res = await request(app)
-  //     .post('/v1/fragments')
-  //     .set('Content-Type', 'text/plain')
-  //     .auth('user1@email.com', 'password1')
-  //     .send('a fragment');
-
-  //   expect(res.statusCode).toBe(201);
-  //   expect(res.body.status).toBe('ok');
-
-  //   const resGet = request(app)
-  //     .get(`/v1/fragments/${res.body.fragment.id}`)
-  //     .auth('user1@email.com', 'password1');
-  //   expect((await resGet).status).toBe(200);
-  // });
-
-  // test('authenticated users receive 404 for missing fragment', async () => {
-  //   const resGet = request(app).get('/v1/fragments/missing').auth('user1@email.com', 'password1');
-  //   expect((await resGet).status).toBe(404);
-  // });
 
   test('convert markdown fragment to HTML', async () => {
     // First, create a markdown fragment
